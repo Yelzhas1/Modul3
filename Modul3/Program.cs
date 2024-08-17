5096e2a525277e020479aa229a32b1fdcf670eed
Yelzhas1 <elzhas_1998@mail.ru>
2024-08-17 22:12:25 +0500

    Добавьте файлы проекта.

---
+++ Modul3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string myName;
+        myName = "Yelzhas";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+    }
+}
