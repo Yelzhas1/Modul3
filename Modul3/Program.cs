e6b4821de1ea341241486de47dde091b25cbcd2c
Yelzhas1 <elzhas_1998@mail.ru>
2024-08-17 22:46:32 +0500

    Итог из 3.2 Переменные, константы, литералы

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -5,11 +5,15 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string myName;
-        myName = "Yelzhas";
+        const string MyName = "Yelzhas";
 
-        Console.WriteLine(myName);
+        Console.WriteLine(MyName);
 
+        Console.WriteLine("\t Привет Мир");
+        Console.WriteLine("\t Мне 26 лет");
+        Console.WriteLine("\t My name is \n {0}", MyName);
+        Console.WriteLine("\u0040");
+        Console.WriteLine("\u0023");
         Console.ReadKey();
     }
 }
