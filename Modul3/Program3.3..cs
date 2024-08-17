98617e3a46c248bbf57819420177ec7a55c3f584
Yelzhas1 <elzhas_1998@mail.ru>
2024-08-17 22:53:26 +0500

    Задание 3.3.2

---
+++ Modul3/Program3.3..cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string MyName = "Yelzhas";
+        byte MyAge = 26;
+        bool HaveIApet = false;
+        double MyShoeSize = 40;
+
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
+    }
+}
